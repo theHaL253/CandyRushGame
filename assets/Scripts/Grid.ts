a9f1643ca0fce7506857fa468fe48a9f777f8135
Đỗ Hoàng Long <longhoangdomelbie2k3@gmail.com>
2023-07-11 17:40:44 +0700

    committt

--- assets/Scripts/Grid.ts
+++ assets/Scripts/Grid.ts
@@ -10,30 +10,50 @@ export class Grid extends Component {
 
     private grid: Node[][] = [];
 
+    @property
+    private tileSpacing: number = 1; // Spacing between tiles
+
+    @property
+    private tileSize: number = 100;
+
     start() {
         this.createGrid();
-        console.log("123");
     }
 
     createGrid() {
-        const tileSize = 100; // Change this according to your desired tile size
+        // const tileSize = 100; // Change this according to your desired tile size
 
         for (let i = 0; i < 7; i++) {
             this.grid[i] = [];
+
             for (let j = 0; j < 7; j++) {
                 const tile = instantiate(this.tilePrefab);
                 tile.parent = this.node;
-                tile.setPosition(i * tileSize, j * tileSize);
 
-                // // Add color to the tile
-                // const tileSprite = tile.getComponent(Sprite);
-                // if (tileSprite) {
-                //     const color = new Color(Math.random() * 255, Math.random() * 255, Math.random() * 255);
-                //     tileSprite.node.color = color;
-                // }
+                // Calculate tile position with spacing
+                const posX = (this.tileSize + this.tileSpacing) * i;
+                const posY = (this.tileSize + this.tileSpacing) * j;
 
+                tile.setPosition(posX, posY);
+
+                // Assign random color to the tile
+                this.assignRandomColor(tile);
+
+                
+                //to assign a tile to the position above
                 this.grid[i][j] = tile;
             }
+        }
+    }
+
+    assignRandomColor(tile: Node) {
+        const colors = [Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN, Color.BLACK];
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        const color = colors[randomIndex];
+
+        const tileSprite = tile.getComponent(cc.Sprite);
+        if (tileSprite) {
+            tileSprite.node.color = color;
         }
     }
 }
