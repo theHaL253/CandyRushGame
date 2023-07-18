b7457d7a362508b46a5dea0d4617e6e34f08d7e4
Đỗ Hoàng Long <longhoangdomelbie2k3@gmail.com>
2023-07-18 17:08:15 +0700

    commmiititt

--- assets/Scripts/Grid.ts
+++ assets/Scripts/Grid.ts
@@ -1,4 +1,5 @@
-import { _decorator, Component, Node, Prefab, instantiate, Sprite, Color, Vec3 } from 'cc';
+import { _decorator, Component, Node, Prefab, instantiate, Sprite, Color, Vec3, v3, find } from 'cc';
+import { Tile } from './Tile';
 const { ccclass, property } = _decorator;
 
 @ccclass('Grid')
@@ -16,13 +17,15 @@ export class Grid extends Component {
     @property
     public tileSize: number = 100;
 
-    @property({
-        type: Node
-    })
-    public tile = null;
+    public game;
 
-    public touchStartPos: Vec3 = new Vec3(0,0,0);
-    public touchCancelPos: Vec3 = new Vec3(0,0,0);
+    public touchStartPos: Vec3 = new Vec3(0, 0, 0);
+    public touchCancelPos: Vec3 = new Vec3(0, 0, 0);
+    public static instance: Grid = null;
+
+    protected onLoad(): void {
+        Grid.instance = this;
+    }
 
     start() {
         this.createGrid();
@@ -35,58 +38,51 @@ export class Grid extends Component {
             this.grid[i] = [];
 
             for (let j = 0; j < 7; j++) {
-                this.tile = instantiate(this.tilePrefab);
-                this.tile.parent = this.node;
+                let tile = instantiate(this.tilePrefab);
+                let tileComponent: Tile = tile.addComponent(Tile);
+                //We have to assign the i, j public variable from Tile.ts file to the current i and j value 
+                tileComponent.i = i;
+                tileComponent.j = j;
+
+                tile.parent = this.node;
 
                 // Calculate tile position with spacing
                 const posX = (this.tileSize + this.tileSpacing) * i;
                 const posY = (this.tileSize + this.tileSpacing) * j;
 
-                this.tile.setPosition(posX, posY);
+                tile.setPosition(posX, posY);
 
                 //Swipe logic of each tile
-                this.tile.on(Node.EventType.TOUCH_START, (eventTouch) => {
+                tile.on(Node.EventType.TOUCH_START, (eventTouch) => {
                     this.touchStartPos = eventTouch.getLocation();
                 })
 
                 //This touch will end at another tile. Not sure if this tile was sticked right way.
-                this.tile.on(Node.EventType.TOUCH_CANCEL, (eventTouch) => {
+                tile.on(Node.EventType.TOUCH_CANCEL, (eventTouch) => {
                     if (!this.touchStartPos) return;
-        
+
                     this.touchCancelPos = eventTouch.getLocation();
                     const deltaX = this.touchCancelPos.x - this.touchStartPos.x;
                     const deltaY = this.touchCancelPos.y - this.touchStartPos.y;
 
-                    // console.log(touchCancelPos, deltaX, deltaY);
-        
                     const thresHoldMin = this.tileSpacing; // Minimum distance for a valid swipe
                     const thresHoldMax = this.tileSize; // Maximum distance for a valid swipe
-        
+
                     const isHorizontalSwipe = Math.abs(deltaX) > Math.abs(deltaY); // I would check the right or left of this swipe later on: Done
                     const isVerticalSwipe = Math.abs(deltaY) > Math.abs(deltaX);
-        
-                    // console.log(isHorizontalSwipe, isVerticalSwipe);
+
                     //Logic Event goes here
                     if (isHorizontalSwipe) {
                         if (deltaX > thresHoldMin && deltaX < thresHoldMax) {
-                            console.log('Swipe right');
-                            // Handle swipe right event
-                            //grid[i+1][j]
-                            this.swipeRight(i,j);
+                            tileComponent.swipeRight();
                         } else if (deltaX < -thresHoldMin && deltaX > -thresHoldMax) {
-                            console.log('Swipe left');
-                            // Handle swipe left event
-                            //grid[i-1][j]
+                            tileComponent.swipeLeft();
                         }
                     } else if (isVerticalSwipe) {
                         if (deltaY > thresHoldMin && deltaY < thresHoldMax) {
-                            console.log('Swipe up');
-                            // Handle swipe up event
-                            //grid[i][j+1]
+                            tileComponent.swipeUp();
                         } else if (deltaY < -thresHoldMin && deltaY > -thresHoldMax) {
-                            console.log('Swipe down');
-                            // Handle swipe down event
-                            //grid[i][j-1]
+                            tileComponent.swipeDown();
                         }
                     } else {
                         return false;
@@ -94,52 +90,28 @@ export class Grid extends Component {
                 })
 
                 //to assign a tile to the position above, important
-                this.grid[i][j] = this.tile;
+                this.grid[i][j] = tile;
 
                 // Assign random color to the tile
-                this.assignRandomColor();
-
-                //Handle swipe for each tile
+                this.assignRandomColor(tile);
             }
         }
     }
 
-    assignRandomColor() {
+    assignRandomColor(tile) {
         const colors = [Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN, Color.MAGENTA];
         const randomIndex = Math.floor(Math.random() * colors.length);
         const color = colors[randomIndex];
 
-        const tileSprite = this.tile.getComponentInChildren(Sprite);
+        const tileSprite = tile.getComponentInChildren(Sprite);
         if (tileSprite) {
             //Not sure about the set color function
             tileSprite.color = color;
         }
     }
 
-    // swipeRight(i,j) {
-    //     const tile1Pos = this.touchStartPos;
-    //     const tile2Pos = this.touchCancelPos;
+
+    selfDetectedCellsCombo() {
         
-    //     this.grid[i][j].setPosition(tile2Pos);
-    //     this.grid[i+1][j].setPosition(tile1Pos);
-
-    //     this.grid[i][j] = this.tile;
-    //     this.grid[i][j+1] = this.tile;
-    // }
-
-    swipeRight(i: number, j: number) {
-        if (i < this.grid.length - 1) {
-            const temp = this.grid[i][j];
-            this.grid[i][j] = this.grid[i+1][j];
-            this.grid[i+1][j] = temp;
-
-            this.grid[i][j].setPosition(this.grid[i][j].getPosition());
-            this.grid[i+1][j].setPosition(this.grid[i+1][j].getPosition());
-
-            this.grid[i][j] = this.tile;
-            this.grid[i+1][j] = this.tile;
-        }
     }
 }
-
-
---
+++ assets/Scripts/Tile.ts
@@ -0,0 +1,125 @@
+import { _decorator, Component, Node, Prefab, instantiate, Sprite, Color, Vec3, v3 } from 'cc';
+import { Grid } from './Grid';
+const { ccclass, property } = _decorator;
+
+@ccclass('Tile')
+export class Tile extends Component {
+    public i: number;
+    public j: number;
+
+    swipeRight() {
+        // console.log(this.grid[i][j].position, this.grid[i + 1][j].position);
+
+        let pos1 = v3(Grid.instance.grid[this.i][this.j].position.x, Grid.instance.grid[this.i][this.j].position.y);
+        let pos2 = v3(Grid.instance.grid[this.i + 1][this.j].position.x, Grid.instance.grid[this.i + 1][this.j].position.y);
+
+        console.log(this.i,this.j);
+        Grid.instance.grid[this.i][this.j].setPosition(pos2);
+        Grid.instance.grid[this.i + 1][this.j].setPosition(pos1);
+        // console.log(this.grid[i][j].position, this.grid[i + 1][j].position)
+
+
+        //Update the grid array
+        const tempTile1 = Grid.instance.grid[this.i][this.j];
+        const tempTile2 = Grid.instance.grid[this.i + 1][this.j];
+        Grid.instance.grid[this.i][this.j] = tempTile2;
+        Grid.instance.grid[this.i + 1][this.j] = tempTile1;
+
+        // const tempTile = Grid.instance.grid[this.i][this.j];
+        // Grid.instance.grid[this.i][this.j] = Grid.instance.grid[this.i + 1][this.j]
+        // Grid.instance.grid[this.i + 1][this.j] = tempTile;
+
+        // // Update indices i and j
+        const tempI = tempTile1.getComponent(Tile).i;
+        const tempJ = tempTile2.getComponent(Tile).j;
+        tempTile1.getComponent(Tile).i = tempTile2.getComponent(Tile).i;
+        tempTile1.getComponent(Tile).j = tempTile2.getComponent(Tile).j;
+        tempTile2.getComponent(Tile).i = tempI;
+        tempTile2.getComponent(Tile).j = tempJ;
+        console.log(this.i,this.j);
+    }
+
+    swipeLeft() {
+        // console.log(this.grid[i][j].position, this.grid[i + 1][j].position);
+
+        let pos1 = v3(Grid.instance.grid[this.i][this.j].position.x, Grid.instance.grid[this.i][this.j].position.y);
+        let pos2 = v3(Grid.instance.grid[this.i - 1][this.j].position.x, Grid.instance.grid[this.i - 1][this.j].position.y);
+
+
+        Grid.instance.grid[this.i][this.j].setPosition(pos2);
+        Grid.instance.grid[this.i - 1][this.j].setPosition(pos1);
+        // console.log(this.grid[i][j].position, this.grid[i + 1][j].position)
+
+
+        //Update the grid array
+        const tempTile1 = Grid.instance.grid[this.i][this.j];
+        const tempTile2 = Grid.instance.grid[this.i - 1][this.j];
+        Grid.instance.grid[this.i][this.j] = tempTile2;
+        Grid.instance.grid[this.i - 1][this.j] = tempTile1;
+
+        // const tempTile = Grid.instance.grid[this.i][this.j];
+        // Grid.instance.grid[this.i][this.j] = Grid.instance.grid[this.i + 1][this.j]
+        // Grid.instance.grid[this.i + 1][this.j] = tempTile;
+
+        // // Update indices i and j
+        const tempI = tempTile1.getComponent(Tile).i;
+        const tempJ = tempTile2.getComponent(Tile).j;
+        tempTile1.getComponent(Tile).i = tempTile2.getComponent(Tile).i;
+        tempTile1.getComponent(Tile).j = tempTile2.getComponent(Tile).j;
+        tempTile2.getComponent(Tile).i = tempI;
+        tempTile2.getComponent(Tile).j = tempJ;
+
+    }
+
+    swipeUp() {
+
+        let pos1 = v3(Grid.instance.grid[this.i][this.j].position.x, Grid.instance.grid[this.i][this.j].position.y);
+        let pos2 = v3(Grid.instance.grid[this.i][this.j+1].position.x, Grid.instance.grid[this.i][this.j+1].position.y);
+
+
+        Grid.instance.grid[this.i][this.j].setPosition(pos2);
+        Grid.instance.grid[this.i][this.j+1].setPosition(pos1);
+
+
+        //Update the grid array
+        const tempTile1 = Grid.instance.grid[this.i][this.j];
+        const tempTile2 = Grid.instance.grid[this.i][this.j+1];
+        Grid.instance.grid[this.i][this.j] = tempTile2;
+        Grid.instance.grid[this.i][this.j+1] = tempTile1;
+
+
+        // // Update indices i and j
+        const tempI = tempTile1.getComponent(Tile).i;
+        const tempJ = tempTile2.getComponent(Tile).j;
+        tempTile1.getComponent(Tile).i = tempTile2.getComponent(Tile).i;
+        tempTile1.getComponent(Tile).j = tempTile2.getComponent(Tile).j;
+        tempTile2.getComponent(Tile).i = tempI;
+        tempTile2.getComponent(Tile).j = tempJ;
+    }
+
+    swipeDown() {
+
+        let pos1 = v3(Grid.instance.grid[this.i][this.j].position.x, Grid.instance.grid[this.i][this.j].position.y);
+        let pos2 = v3(Grid.instance.grid[this.i][this.j-1].position.x, Grid.instance.grid[this.i][this.j-1].position.y);
+
+
+        Grid.instance.grid[this.i][this.j].setPosition(pos2);
+        Grid.instance.grid[this.i][this.j-1].setPosition(pos1);
+
+
+        //Update the grid array
+        const tempTile1 = Grid.instance.grid[this.i][this.j];
+        const tempTile2 = Grid.instance.grid[this.i][this.j-1];
+        Grid.instance.grid[this.i][this.j] = tempTile2;
+        Grid.instance.grid[this.i][this.j-1] = tempTile1;
+
+
+        // // Update indices i and j
+        const tempI = tempTile1.getComponent(Tile).i;
+        const tempJ = tempTile2.getComponent(Tile).j;
+        tempTile1.getComponent(Tile).i = tempTile2.getComponent(Tile).i;
+        tempTile1.getComponent(Tile).j = tempTile2.getComponent(Tile).j;
+        tempTile2.getComponent(Tile).i = tempI;
+        tempTile2.getComponent(Tile).j = tempJ;
+    }
+}
